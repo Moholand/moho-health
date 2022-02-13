8e3926d0e6f02d4ea19d7199b40c6c86ca3fdfac
Moholand <moho.akbarzadeh@gmail.com>
2022-02-13 12:22:14 +0330

    add logo to department seeder file

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -13,18 +13,19 @@ class DepartmentSeeder extends Seeder
      */
     public function run()
     {
-        $departmentNames = [
-            'name' => 'بخش چشم ‌پزشکی', 
-            'بخش دندان ‌پزشکی', 
-            'بخش مغز و اعصاب',
-            'بخش جراحی',
-            'بخش قلب',
-            'بخش اطفال'
+        $departments = [
+            ['name' => 'بخش چشم ‌پزشکی', 'logo' => 'fas fa-glasses'],
+            ['name' => 'بخش دندان ‌پزشکی', 'logo' => 'fas fa-tooth'],
+            ['name' => 'بخش مغز و اعصاب', 'logo' => 'fas fa-brain'],
+            ['name' => 'بخش جراحی', 'logo' => 'fa fa-user-md'],
+            ['name' => 'بخش قلب', 'logo' => 'fa fa-heartbeat'],
+            ['name' => 'بخش اطفال', 'logo' => 'fas fa-baby'],
         ];
 
-        foreach($departmentNames as $departmentName) {
+        foreach($departments as $department) {
             $department = \App\Models\Department::factory()->create([
-                'name' => $departmentName
+                'name' => $department['name'],
+                'logo' => $department['logo']
             ]);
 
             // For each department create 10 doctors
